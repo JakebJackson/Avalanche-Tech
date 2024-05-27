71c3e2d86504c4e1e37aec0ebbcc48e5b9d69e02
Jake Jackson <142851548+JakebJackson@users.noreply.github.com>
2024-05-27 10:28:07 +1000

    Protected overlooked environment variables.

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,7 +1,9 @@
 const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const secret = process.env.SECRET_KEY;
 
-const secret = 'mysecretssshhhhhhh';
+
 const expiration = '2h';
 
 module.exports = {
